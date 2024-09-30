8a1b7e386f856f9b9f9d9a30135caf9e9a6042a4
manianbu14 <manimozhianbalagan1014@gmail.com>
2024-09-30 16:01:54 +0530

    add file

---
+++ src/Pages/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <p>Explore our delicious offerings.</p>
+    </div>
+  );
+}
+
+export default Menu;
